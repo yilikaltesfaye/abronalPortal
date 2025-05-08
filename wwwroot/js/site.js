463df2ce333a59e9e03061f9fdc2db01151a3d00
Yilikal Tesfaye <yilikalt.kebede@gmail.com>
2025-05-08 17:16:33 +0300

    removed bootstrap from the project

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,12 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function toggleMobileMenu() {
+    const mobileMenu = document.getElementById('mobile-menu');
+    if (mobileMenu.style.display === 'none' || mobileMenu.style.display === '') {
+        mobileMenu.style.display = 'block';
+    } else {
+        mobileMenu.style.display = 'none';
+    }
+}
