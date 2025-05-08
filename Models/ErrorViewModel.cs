e2383887346cd9a678c96c48df2fd8b8083972ab
Yilikal Tesfaye <yilikalt.kebede@gmail.com>
2025-05-08 15:05:21 +0300

    Initialized asp.net core 8 project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace abronalPortal.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
