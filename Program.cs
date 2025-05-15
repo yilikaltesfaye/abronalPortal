1ec31fd382d3b7f492289f34ae00736170cbbc5d
Yilikal Tesfaye <yilikalt.kebede@gmail.com>
2025-05-15 19:48:05 +0300

    fix(auth): update password policy to allow non-alphanumeric characters

--- Program.cs
+++ Program.cs
@@ -17,6 +17,7 @@ builder.Services.AddIdentity<Users, IdentityRole>(options =>
     options.Password.RequireDigit = true;
     options.Password.RequireLowercase = true;
     options.Password.RequiredLength = 8;
+    options.Password.RequireNonAlphanumeric = false;
     options.User.RequireUniqueEmail = true;
     options.SignIn.RequireConfirmedAccount = false;
     options.SignIn.RequireConfirmedEmail = false;
