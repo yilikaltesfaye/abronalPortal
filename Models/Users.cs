a7eb51c066b05f0b2f632a32faf209bbd76e66d3
Yilikal Tesfaye <yilikalt.kebede@gmail.com>
2025-05-15 14:18:53 +0300

    feat(migrations): add initial migration and model snapshot for identity tables

---
+++ Models/Users.cs
@@ -0,0 +1,11 @@
+
+using Microsoft.AspNetCore.Identity;
+
+namespace abronalPortal.Models
+{
+    public class Users : IdentityUser
+    {
+        public string Fullname { get; set;}
+        public string ProfilePicturePath { get; set;}
+    }
+}
