755706486acae0e7e1067c7cc5786e2af2e43281
Yilikal Tesfaye <yilikalt.kebede@gmail.com>
2025-05-15 19:49:05 +0300

    feat(migration): make ProfilePicturePath nullable in AspNetUsers table

---
+++ Migrations/20250515164256_MakeProfilePicturePathNullable.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace abronalPortal.Migrations
+{
+    /// <inheritdoc />
+    public partial class MakeProfilePicturePathNullable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ProfilePicturePath",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ProfilePicturePath",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
