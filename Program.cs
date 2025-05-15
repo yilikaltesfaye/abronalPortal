7c0cb13664c84853a7f18cc25c7193028b8e92f9
Yilikal Tesfaye <yilikalt.kebede@gmail.com>
2025-05-15 11:03:48 +0300

    refactor(models): remove obsolete model classes from the project

--- Program.cs
+++ Program.cs
@@ -6,9 +6,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-builder.Services.AddDbContext<AbronalPortalDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
 
 var app = builder.Build();
 
